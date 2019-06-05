ace463df27a592b4885de11dbd6779f47b56f0c9
dev-florian <f.the@sfr.fr>
2019-06-05 16:43:59 +0200

    first commit

---
+++ src/views/board/Board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './_index.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <button onClick={() => alert('hello')}>Let's go</button>
+        </div>
+    );
+}
+
+export default App;
