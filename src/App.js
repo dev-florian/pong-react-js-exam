bbed4392bfa3cb9326c4c4a21c2558126d009d37
dev-florian <f.the@sfr.fr>
2019-06-05 16:45:00 +0200

    V1 jeu pong

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Board from "./views/board/Board";
 
 function App() {
-  return (
-    <div className="App">
-        <button onClick={() => alert('hello')}>Let's go</button>
-    </div>
-  );
+    return (
+        <Board/>
+    );
 }
 
 export default App;
--- src/views/board/Board.js
+++ src/views/board/Board.js
@@ -1,12 +1,171 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './_index.scss';
 
-function App() {
-    return (
-        <div className="App">
-            <button onClick={() => alert('hello')}>Let's go</button>
-        </div>
-    );
+class Board extends Component {
+
+    constructor() {
+        super();
+
+        //Positionnement de la barre
+        this.paddle1Y = 300;
+        this.speed = 0;
+
+        //Position de la ball
+        this.ballX = 400;
+        this.ballY = 300;
+
+        this.state = {
+            //Taille de la barre
+            paddleHeight: 150,
+            paddleWidth: 15,
+
+            //Refresh par seconde
+            framesPerSecond: 50,
+
+            //Score du joueur
+            score: 0,
+
+            //clicked boutton central
+            clicked: false,
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener("keydown", this.onKeyDown.bind(this));
+        document.addEventListener("keyup", this.onKeyUp.bind(this));
+    }
+
+
+    launchPong() {
+        this.setState({clicked: true});
+
+        //Ajout d'un canvas
+        const canvas = this.refs.canvas;
+        const canvasContext = this.refs.canvas.getContext('2d');
+
+        //Vitesse de la ball
+        let ballSpeedX = 15;
+        let ballSpeedY = 15;
+
+
+        //On dessine un paddle
+        const makeRectangleShape = (cX, cY, width, height, color) => {
+            canvasContext.fillStyle = color;
+            canvasContext.fillRect(cX, cY, width, height);
+        };
+
+        //On dessine un cercle
+        const makeCircleShape = (cX, cY, radious, angle, color) => {
+            canvasContext.fillStyle = color;
+            canvasContext.beginPath();
+            canvasContext.arc(cX, cY, radious, angle, Math.PI * 2, true);
+            canvasContext.fill();
+        };
+
+
+        //Dessiner
+        const draw = () => {
+            canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+            //Construction du paddle
+            makeRectangleShape(10, this.paddle1Y, this.state.paddleWidth, this.state.paddleHeight);
+
+            //Construction de la ball
+            makeCircleShape(this.ballX, this.ballY, 50, 0, "black");
+        };
+
+        //On fait bouger les elements du canvas
+        const move = () => {
+
+            this.paddle1Y = this.paddle1Y + this.speed;
+
+            //ELEMENT BALL
+            this.ballX += ballSpeedX;
+            this.ballY += ballSpeedY;
+
+            //Estimons que nous somme du coté gauche
+            //Si la balle est vers notre côté
+            if (this.ballX < 75) {
+                //Si la paddle touche la ball
+                if ((this.ballY > this.paddle1Y) && (this.ballY < this.paddle1Y + this.state.paddleHeight)) {
+                    //On tape un rebond
+                    ballSpeedX = -ballSpeedX;
+
+                    //Modification de la vitesse de renvoit
+                    let deltaY = this.ballY - (this.paddle1Y + this.state.paddleHeight / 2);
+                    ballSpeedY = deltaY * 0.60;
+                } else {
+                    this.setState({score: this.state.score + 1});
+                    ballReset();
+                }
+            }
+
+            //Faire rebondir la balle
+
+            //Rebond en haut
+            if (this.ballY > 550) {
+                ballSpeedY = -ballSpeedY
+            }
+
+            //Rebond en bas
+            if (this.ballY < 50) {
+                ballSpeedY = -ballSpeedY
+            }
+
+            //Rebond à droite
+            if (this.ballX > 750) {
+                ballSpeedX = -ballSpeedX
+            }
+
+        };
+
+        //On replace la ball au milieu
+        const ballReset = () => {
+            this.ballX = canvas.width / 2;
+            this.ballY = canvas.height / 2;
+            ballSpeedX = -ballSpeedX;
+        };
+
+
+        setInterval(() => {
+            draw();
+            move();
+        }, 1000 / this.state.framesPerSecond);
+    }
+
+    //ELEMENT PADDLE
+    onKeyDown(e) {
+        //Si touche du bas
+        if (e.keyCode === 40) {
+            if (this.paddle1Y < 550) {
+                this.speed = 20;
+            }
+        }
+
+        //Si touche du haut
+        if (e.keyCode === 38) {
+            if (this.paddle1Y > 50) {
+                this.speed = -20;
+            }
+        }
+    }
+
+    onKeyUp(e) {
+        this.speed = 0;
+    }
+
+    render() {
+        return (
+            <div className="Board">
+                <button style={{x: this.ballX, y: this.ballY}} className={this.state.clicked ? "clicked" : ""}
+                        onClick={() => this.launchPong()}>
+                </button>
+                <canvas ref="canvas" width={800} height={600}/>
+                <div className="data">
+                    <p className="score">Total defaite : {this.state.score}</p>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Board;
